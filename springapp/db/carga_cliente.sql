a4abba49a60e0d59eb324e3445c79c902346e908
Manu <aspiacc@hotmail.com>
2019-11-27 13:39:34 -0300

    scripts de carga de datos factura y cliente

---
+++ springapp/db/carga_cliente.sql
@@ -0,0 +1,3 @@
+insert into cliente (razon_social, cuit, telefono, codigo_postal, direccion_id) values ('Manuel S.A', '20330297037', '4681123', '5000', 1);
+insert into cliente (razon_social, cuit, telefono, codigo_postal, direccion_id) values ('Test S.A', '206666667', '123456789', '5000', 2);
+insert into cliente (razon_social, cuit, telefono, codigo_postal, direccion_id) values ('Prueba SRL', '666', '3515484898', '5000', 2);
---
+++ springapp/db/carga_factura.sql
@@ -0,0 +1,4 @@
+insert into factura (fecha_emision, fecha_vencimiento, fact_sub_total, estado, cliente_id) 
+values ('2018-05-12', '2019-05-12', 1000, 'impago', 1);
+insert into factura (fecha_emision, fecha_vencimiento, fact_sub_total, estado, cliente_id) 
+values ('2018-05-12', '2019-05-12', 1000, 'impago', 2);
