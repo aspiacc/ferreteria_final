9b253a429f20b5bc8cb02efea44fb800e969dae0
Manu <aspiacc@hotmail.com>
2019-11-27 13:25:39 -0300

    script de carga producto y tipo iva

---
+++ springapp/db/carga_producto.sql
@@ -0,0 +1,5 @@
+insert into producto (stock, descripcion, precio_unitario, tipo_iva_id) values (3, 'Bebida', 50.50, 1);
+insert into producto (stock, descripcion, precio_unitario, tipo_iva_id) values (2, 'Comida', 20.20, 1);
+insert into producto (stock, descripcion, precio_unitario, tipo_iva_id) values (10, 'Coca Cola', 90, 1);
+insert into producto (stock, descripcion, precio_unitario, tipo_iva_id) values (2, 'Carne', 300, 2);
+insert into producto (stock, descripcion, precio_unitario, tipo_iva_id) values (1, 'Whisky', 90, 3);
---
+++ springapp/db/carga_tipo_iva.sql
@@ -0,0 +1,3 @@
+insert into tipo_iva (descripcion, alicuota) values ('21%', 1.21);
+insert into tipo_iva (descripcion, alicuota) values ('10%', 1.1);
+insert into tipo_iva (descripcion, alicuota) values ('4%', 0.4);
